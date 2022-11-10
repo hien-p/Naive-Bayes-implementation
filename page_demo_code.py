6b5cbbbbdd2e116f510c2dfbf24b848050654116
hien-p <hphan0390@gmail.com>
2022-11-10 14:22:33 +0700

    update

--- page_demo_code.py
+++ page_demo_code.py
@@ -31,6 +31,19 @@ st.page_config = st.set_page_config(
     initial_sidebar_state="expanded",
 )
 
+hide_menu = """
+<style>
+#MainMenu {
+    visibility:hidden;
+    
+}
+
+footer{
+    visibility:hidden;
+}
+</style>
+"""
+st.markdown(hide_menu, unsafe_allow_html=True)
 
 def button_created(key):
     """
